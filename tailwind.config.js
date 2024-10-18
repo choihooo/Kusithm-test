5ad6bf51636afbcba1ce4b3597437a4371ecf64e
choihooo <hochoi8621@naver.com>
2024-10-18 19:52:55 +0900

    테일윈드 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // src 폴더 내부의 모든 파일들에 대해 적용
+  ],
   theme: {
     extend: {},
   },
