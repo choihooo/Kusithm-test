cae73b59dd83cad3529d71be9606142cbcb2c010
choihooo <hochoi8621@naver.com>
2024-11-26 04:31:04 +0900

    frame svg에서 png로 변경

--- src/pages/Result/Result.jsx
+++ src/pages/Result/Result.jsx
@@ -98,7 +98,7 @@ function Result() {
 
         <div className="absolute top-[80px] left-1/2 transform -translate-x-1/2 w-[340px] h-[340px]">
           <img
-            src="/Frame.png"
+            src="/Frame.svg"
             alt="원소 바탕 이미지"
             className="absolute object-contain w-full h-full"
             onLoad={handleImageLoad}
